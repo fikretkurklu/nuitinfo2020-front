aacc6fb5c06a7a334fa09ce1b391869b5021a4c9
Malone JULIENNE <malone.julienne@etu.univ-grenoble-alpes.fr>
2020-12-04 04:23:51 +0100

    routing - terminated

--- nuit-info-surfapp/src/app/app-routing.module.ts
+++ nuit-info-surfapp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageConnexionComponent } from './page-connexion/page-connexion.component';
+import { PageInscriptionComponent } from './page-inscription/page-inscription.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'home', component : PageConnexionComponent },
+  { path : '',   redirectTo: '/home', pathMatch: 'full' },
+  { path : 'register', component : PageInscriptionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
