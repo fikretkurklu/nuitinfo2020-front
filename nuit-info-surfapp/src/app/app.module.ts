89b976ca2dc181b744fe46cdea18ecdfc8f3af0c
Louise.mathe <Louise.mathe@live.fr>
2020-12-04 01:29:52 +0100

    sensibilisation nul

--- nuit-info-surfapp/src/app/app.module.ts
+++ nuit-info-surfapp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageConnexionComponent } from './page-connexion/page-connexion.component';
+import { SensibilisationComponent } from './sensibilisation/sensibilisation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageConnexionComponent
+    PageConnexionComponent,
+    SensibilisationComponent
   ],
   imports: [
     BrowserModule,
