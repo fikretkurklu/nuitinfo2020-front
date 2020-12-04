969bc5eff674807a2c462289c89056f06e8f8806
Guillaume <guillaume.mlln@gmail.com>
2020-12-04 08:01:36 +0100

    ff

--- nuit-info-surfapp/src/app/app-routing.module.ts
+++ nuit-info-surfapp/src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PageConnexionComponent } from './page-connexion/page-connexion.component';
 import { PageInscriptionComponent } from './page-inscription/page-inscription.component';
+import { AcceuilComponent } from './acceuil/acceuil.component'; 
+import { SanteComponent } from './sante/sante.component';
+import { photoprofilComponent } from './photoprofil/photoprofil.component';
+import { InfopageComponent } from './infopage/infopage.component';
 
 const routes: Routes = [
+  { path : 'acceuil', component : AcceuilComponent },
+  { path : '',   redirectTo: '/acceuil', pathMatch: 'full' },
   { path : 'home', component : PageConnexionComponent },
-  { path : '',   redirectTo: '/home', pathMatch: 'full' },
-  { path : 'register', component : PageInscriptionComponent}
+  { path : 'sante', component : SanteComponent },
+  { path : 'photoprofil', component : photoprofilComponent },
+  { path : 'infopage', component : InfopageComponent },
+  { path : 'register', component : PageInscriptionComponent},
+  { path : 'page-connexion', component : PageConnexionComponent },
+  { path : 'page-inscription', component : PageInscriptionComponent }
 ]
 
 @NgModule({
