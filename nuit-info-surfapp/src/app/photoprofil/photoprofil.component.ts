abaf6be765fe281d37fdb781bbb7891e4e27e133
T <tahirines00@gmail.com>
2020-12-04 02:49:39 +0100

    Page profil

---
+++ nuit-info-surfapp/src/app/photoprofil/photoprofil.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photoprofil',
+  templateUrl: './photoprofil.component.html',
+  styleUrls: ['./photoprofil.component.css']
+  
+})
+export class photoprofilComponent implements OnInit {
+  profilePicture: string = 'ajout';
+
+  constructor() { 
+  }
+  
+  ngOnInit(): void {
+   
+
+  }}
+
